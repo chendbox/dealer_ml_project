9c24fd526e1f486c6a008405ad9de61ca8332d57
chendbox <chendbox@gmail.com>
2023-05-24 12:19:15 -0400

    EDA and prediction

--- src/exception.py
+++ src/exception.py
@@ -21,10 +21,10 @@ class CustomException(Exception):
         return self.error_message
     
     
-if __name__ == "__main__":
-        try:
-                a = 1/0
-        except Exception as e:
-            #    logging.info("Logging has started")
-               raise CustomException(e, sys)
+# if __name__ == "__main__":
+#         try:
+#                 a = 1/0
+#         except Exception as e:
+#             #    logging.info("Logging has started")
+#                raise CustomException(e, sys)
                
